davekch/kmtracker

commit 910b40033f4a2c15bda7f1a60d90b3e784acb280

sort newest rides on top

kmtracker/db.py
@@ -190,7 +190,7 @@ def get_latest_entries(connection: sqlite3.Connection, n: int) -> list[sqlite3.R
     connection.row_factory = sqlite3.Row
     with closing(connection.cursor()) as cursor:
         latest = cursor.execute(
-            f"{Rides.SELECT_ALL} ORDER BY {Rides.columns.timestamp} DESC LIMIT ?",
+            f"{Rides.SELECT_ALL} ORDER BY {Rides.columns.timestamp} ASC LIMIT ?",
             (n,)
         ).fetchall()
         return list(reversed(latest))

commit 045f7a004df2a64892ea5371f477a028c04b2363

fix ls -n
regression bug introduced in 910b40033

kmtracker/db.py
@@ -189,11 +189,10 @@ def get_latest_entries(connection: sqlite3.Connection, n: int) -> list[sqlite3.R
     """
     connection.row_factory = sqlite3.Row
     with closing(connection.cursor()) as cursor:
-        latest = cursor.execute(
-            f"{Rides.SELECT_ALL} ORDER BY {Rides.columns.timestamp} ASC LIMIT ?",
+        return cursor.execute(
+            f"{Rides.SELECT_ALL} ORDER BY {Rides.columns.timestamp} DESC LIMIT ?",
             (n,)
         ).fetchall()
-        return list(reversed(latest))
 
 
 def get_gpx(connection: sqlite3.Connection, id: int) -> str | None: